20722442addaf427b0d0ff16161bd6f68194c41c
Benjamin Ng <BENJAMINNG.22@ichat.sp.edu.sg>
2023-10-30 16:26:59 +0800

    Lab3 and Test_Lab3 modifications

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -21,10 +21,25 @@ def test_bubble_sort_descending():
 
     assert (result == test_arr)
 
-def test_bubble_sort_invalid():
+def test_bubble_sort_empty():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = []
 
-    result = Lab3.bubble_sort(input_arr, 3)
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
-    assert (result == [])
+    assert (result == 0)
+def test_bubble_sort_morethanTen():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 10, 10, 10, 10]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 1)
+
+def test_bubble_sort_notInteger():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 10, 10, 10, 10.1]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
+
+    assert (result == 2)
