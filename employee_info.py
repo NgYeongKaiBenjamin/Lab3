35899cd2419078e4d48b637d7c1beacd80d29d10
Benjamin Ng <BENJAMINNG.22@ichat.sp.edu.sg>
2023-11-02 21:01:56 +0800

    Completed get employees by dept function

--- employee_info.py
+++ employee_info.py
@@ -21,12 +21,10 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-    count=0
 
     for salary in employee_data:
-        total=salary["salary"]
-        count+=1
-    average=total/count
+        total+=salary["salary"]
+    average=total/len(employee_data)
 
     return average
 
